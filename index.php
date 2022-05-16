26c730f6d83511fdb6657d77660d5d16eaddf194
ARS-TECNOLOGIC <anselmoroberto003@gmail.com>
2022-05-16 20:46:26 -0300

    Atualização Tela Login

--- index.php
+++ index.php
@@ -9,31 +9,33 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/estilo_login.css">
+    <link rel="stylesheet" href="css/fonts.css">
     <script src="https://kit.fontawesome.com/15b4b8b90e.js"></script>
 </head>
 
 <body>
-    <div class="container tamanho-largura">
-        <h2 class="text-center">ACESSO RESTRITO</h2>
-        <div class="d-flex justify-content-center">
-            <img src="img/restrito.png" width="125px" height="125px" alt="">
+    <div class="main-login">
+        <div class="container tamanho-largura">
+            <h2 class="text-center text-titulo-login">ACESSO RESTRITO</h2>
+            <div class="d-flex justify-content-center">
+                <img src="img/imgTelaLogin.png" width="270px" height="178px" alt="">
+            </div>
+            <form action="logar.php" method="POST" class="log-form">
+                <div class="form-group">
+                    <label class="log-user"> Login do Usuário</label>
+                    <input class="form-control log-user" type="text" name="usuario" placeholder="Digite o seu login" autocomplete="off" required />
+                </div>
+
+                <div class="form-group">
+                    <label class="log-pass">Senha</label>
+                    <input class="form-control log-pass" type="password" name="senha" placeholder="Digite sua senha" autocomplete="off" required />
+                </div>
+                <button type="submit" class="log-btn-entrar">Entrar</button>
+            </form><br>
+          
+
         </div>
-        <form action="logar.php" method="POST">
-            <div class="form-group">
-                <strong><label> Login do Usuário</label></strong>
-                <input class="form-control" type="text" name="usuario" placeholder="Digite o seu login"
-                    autocomplete="off" required />
-            </div>
-
-            <div class="form-group">
-                <strong><label>Senha</label></strong>
-                <input class="form-control" type="password" name="senha" placeholder="Digite sua senha"
-                    autocomplete="off" required />
-            </div>
-            <button type="submit" class="btn btn-primary btn-lg btn-block " style="background-color: red; border:none">Entrar</button>        
-        </form><br>
-        <a href="../index.php" class="btn btn-info btn-lg" style="background-color: black; border:none" role="button" aria-disabled="true">Home</a>
-
     </div>
 </body>
+
 </html>
